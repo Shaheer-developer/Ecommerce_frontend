c881d2db2b1ab1cafb03384e187a898ab0a470d1
Shaheer-developer <m.shaheer062006@gmail.com>
2024-11-29 23:45:40 +0500

    made the navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect ,useContext} from 'react'
+import React, { useState, useEffect, useContext, useRef } from 'react'
 import './Navbar.css'
 import logo from '../assets/logo.png'
 import cart_icon from '../assets/cart_icon.png'
@@ -10,34 +10,62 @@ import { ShopContext } from "../../context/Shopcontext";
 
 const Navbar = () => {
     const { getTotalCartItems } = useContext(ShopContext)
-    const [menu,setMenu]=useState("Shop");
-    const location=useLocation();
-useEffect(()=>{
-    const currentpath=location.pathname.split("/")[1];
-    setMenu(currentpath || 'Shop');
-},[location.pathname]);
+    const [menu, setMenu] = useState("Shop");
+    const [isopen, setisopen] = useState("false")
+    const location = useLocation();
+    useEffect(() => {
+        const currentpath = location.pathname.split("/")[1];
+        setMenu(currentpath || 'Shop');
+        setisopen(false);
+        menuRef.current?.classList.remove("nav-menu-visible");
+    }, [location.pathname]);
+    const menuRef = useRef();
+
+    const dropdown_toggle = () => {
+        setisopen(!isopen)
+        menuRef.current.classList.toggle("nav-menu-visible")
+    }
+    const handleMenuClick = (menuName) => {
+        setMenu(menuName);
+        setisopen(false);
+        menuRef.current?.classList.remove("nav-menu-visible");
+    };
+
     return (
         <>
-        <div className='navbar'>
-            <div className="nav-logo">
-                <img src={logo} alt="" />
-                <p>ESSENTIA</p>
-            </div>
-            <ul className="nav-menu">
-                <li onClick={()=>{setMenu("Shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==="Shop"? <hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}}  to='/mens'>Men</Link>{menu==="mens"? <hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("womens")}} ><Link style={{textDecoration:'none'}}  to='/womens'>Women</Link> {menu==="womens"? <hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration:'none'}}  to='/kids'>Kids</Link>  {menu==="kids"? <hr/>:<></>}</li>
-            </ul>
-            <div className="nav-login-cart">
-               <Link style={{textDecoration:'none'}}  to='/login'><button>Login</button></Link> 
-               <Link style={{textDecoration:'none'}}  to='/cart'> <img src={cart_icon} alt="" /></Link>
-                <div className="nav-cart-count">{getTotalCartItems()}</div>
+            <div className='navbar'>
+                <div className="nav-logo">
+                    <img src={logo} alt="" />
+                    <p>ESSENTIA</p>
+                </div>
+                <ul ref={menuRef} className="nav-menu">
+                    <li onClick={() => { handleMenuClick("Shop") }}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu === "Shop" ? <hr /> : <></>}</li>
+                    <li onClick={() => { handleMenuClick("mens") }}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>{menu === "mens" ? <hr /> : <></>}</li>
+                    <li onClick={() => { handleMenuClick("womens") }} ><Link style={{ textDecoration: 'none' }} to='/womens'>Women</Link> {menu === "womens" ? <hr /> : <></>}</li>
+                    <li onClick={() => { handleMenuClick("kids") }}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>  {menu === "kids" ? <hr /> : <></>}</li>
+                </ul>
+
+
+                <div className="nav-login-cart">
+                    <button
+                        className={`hamburger ${isopen ? "open" : ""}`}
+                        onClick={dropdown_toggle}>
+
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                    </button>
+
+                    <Link style={{ textDecoration: 'none' }} to='/login'><button className='loginbutton'>Login</button></Link>
+                    <Link style={{ textDecoration: 'none' }} to='/cart'> <img src={cart_icon} alt="" /></Link>
+                    <div className="nav-cart-count">{getTotalCartItems()}</div>
+
+
+                </div>
+
 
             </div>
-
-        </div>
-        <hr/>
+            <hr />
         </>
 
 
